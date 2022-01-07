cadd1b2969d670768e1d0e1b33a7607c46d6b040
CampanelliEmanuele <emanuele.campanelli@studio.unibo.it>
2022-01-07 02:29:49 +0100

    Bump: code 4 1 shift register

--- 74HC595_shift-register/scripts/1-shift-register_test.ino
+++ 74HC595_shift-register/scripts/1-shift-register_test.ino
@@ -1,17 +1,22 @@
-const int clockPin = 13;    // SH_CP   i pin dello shift register per la  
-const int latchPin = 12;    // ST_CP   gestione delle uscita dello shift register
-const int dataPin = 11;     // ds
+#define DataPin 11     // ds
+#define LatchPin 12    // ST_CP   
+#define ClockPin 13    // SH_CP   i pin dello shift register per la gestione delle uscita dello shift register
 
-void setup() {
-    pinMode(clockPin, OUTPUT);   // vengono dichiarati come output
-    pinMode(latchPin, OUTPUT);
-    pinMode(dataPin, OUTPUT);
+/*
+*/
 
-    digitalWrite(latchPin, LOW);                   // si disattiva il pin latch per scrivere sulle uscite Q0-Q7
-    shiftOut(dataPin, clockPin, MSBFIRST, 0xff);   // il valore delle uscite viene scritto in esadecimare
-    digitalWrite(latchPin, HIGH);                  // si attiva il pin latch dello shift register
+void setup () {
+    pinMode(ClockPin, OUTPUT);
+    pinMode(LatchPin, OUTPUT);
+    pinMode(DataPin, OUTPUT);
+    
+    digitalWrite(LatchPin, LOW);
+    // il valore delle uscite viene scritto in esadecimare
+    shiftOut(DataPin, ClockPin, MSBFIRST, 0xff);
+    digitalWrite(LatchPin, HIGH);
+
 }
 
-void loop() {  
-    
+void loop() {
+
 }
